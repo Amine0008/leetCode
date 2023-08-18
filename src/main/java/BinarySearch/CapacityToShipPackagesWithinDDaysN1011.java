1dd1dc9289e1a7579ae8883472279581aab8b22a
aminef <fouratiamine5@gmail.com>
2023-08-18 01:11:18 +0100

    leecode-binarysearch

---
+++ src/main/java/BinarySearch/CapacityToShipPackagesWithinDDaysN1011.java
@@ -0,0 +1,65 @@
+package BinarySearch;
+
+public class CapacityToShipPackagesWithinDDaysN1011 {
+	
+	public static void main(String[] args) {
+//		int[] input = new int[] {1,2,3,4,5,6,7,8,9,10};
+//		shipWithinDays(input,5);
+	
+	}
+	
+    public int shipWithinDays(int[] weights, int days) {
+     
+    	int weightsSum = 0;
+    	int minCapacity = weights[0];
+    	for(int i = 0; i < weights.length; i++) {
+    		weightsSum += weights[i];
+    		if(weights[i] > minCapacity)
+    			minCapacity = weights[i];
+    	}
+    	
+    	//weightsSum == MAX CAPACITY
+    	
+    	int left = minCapacity;
+    	int right = weightsSum;
+    	int possibleCapacity = 0;
+    	int mid;
+    	
+    	while(left <= right) {
+    		mid = (right-left) / 2 + left;
+    	
+    	if(canShipWithCapacity(mid, weights, days)) {
+    		possibleCapacity = mid;
+    		right = mid-1;
+    	}
+    	else
+    		left = mid+1;
+    		
+    		
+    		
+    	}
+    	return possibleCapacity;
+    	
+    	
+    }
+    
+    public boolean  canShipWithCapacity(int capacity, int[] weights, int  days) {
+    	int numberOfDays = 1;
+    	int currentLoad = 0;
+    	
+    	for(int i = 0; i < weights.length; i++) {
+    		if(currentLoad+weights[i] > capacity) {
+    			numberOfDays++;
+    			currentLoad=weights[i];
+    		}
+    		else {
+    			currentLoad+=weights[i];
+    		}
+    	}
+    	return numberOfDays <= days; 
+    	
+    }
+
+    
+   
+}
---
+++ src/main/java/BinarySearch/FindPeakElementN162.java
@@ -0,0 +1,30 @@
+package BinarySearch;
+
+public class FindPeakElementN162 {
+	
+    public int findPeakElement(int[] nums) {
+    	int left = 0; 
+    	int right = nums.length - 1;
+    	int mid;
+    	if(nums.length == 1)
+    		return 0;
+    	
+    	if(nums[left]>nums[left+1])
+    		return left;
+    	if(nums[right]>nums[right-1])
+    		return right;
+    	
+    	while(left <= right) {
+    		mid = (right - left) / 2 + left;
+    		if(nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1])
+    			return mid;
+    		if(nums[mid+1]>nums[mid])
+    			left = mid+1;
+    		else
+    			right = mid-1;
+    	}
+    	
+    	return -1;
+    }
+
+}
