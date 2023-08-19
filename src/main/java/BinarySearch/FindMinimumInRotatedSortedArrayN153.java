3a8fd91049e138bb611177cb5826ce66ace8e34e
aminef <fouratiamine5@gmail.com>
2023-08-19 03:42:15 +0100

    leetcode-binarysearch

---
+++ src/main/java/BinarySearch/FindMinimumInRotatedSortedArrayN153.java
@@ -0,0 +1,46 @@
+package BinarySearch;
+
+public class FindMinimumInRotatedSortedArrayN153 {
+
+	
+	public static void main(String[] args) {
+		int[] nums = new int[] {1};
+		
+	//	System.out.println(findPivotIndex(nums));
+	}
+	
+	public int findPivotIndex(int[] nums) {
+		if(nums.length == 1)
+			return -1;
+		int left = 0;
+		int right = nums.length-1;
+		int mid;
+		
+		if(nums[0]>nums[1])
+			return 0;
+		if(nums[right]<nums[right-1])
+			return right-1;
+		
+		while(left<=right) {
+			mid = (right-left)/2+left;
+			if(mid+1 >= nums.length || mid-1< 0)
+				return - 1;
+			if(nums[mid]<nums[mid-1])
+				return mid-1;
+			if(nums[mid]>nums[mid+1])
+				return mid;
+			if(nums[mid]>=nums[left])
+				left = mid + 1;
+			else
+				right = mid - 1;
+		}
+		return - 1; 
+	}
+
+    public  int findMin(int[] nums) {
+        int pivotIndex = findPivotIndex(nums);
+        if(pivotIndex == -1)
+        	return nums[0];
+        return nums[pivotIndex+1];
+    }
+}
--- src/main/java/BinarySearch/LeetcodeN875.java
+++ src/main/java/BinarySearch/LeetcodeN875.java
@@ -4,11 +4,45 @@ public class LeetcodeN875 {
 
 	public static void main(String[] args) {
 
+		int[] piles = new int[]{312884470};
+		minEatingSpeed(piles,312884469);
+		
 	}
 	
-    public int minEatingSpeed(int[] piles, int h) {
+    public static int minEatingSpeed(int[] piles, int h) {
+        int maxSpeed = piles[0];
+        for(int i = 0; i<piles.length; i++) {
+        	if(piles[i] > maxSpeed)
+        		maxSpeed = piles[i];
+        }
         
-    	return -1;	
+        int left = 1;
+        int right = maxSpeed;
+        int mid;
+        int result = 1;
+        int hoursTaken;
+        while(left <= right) {
+        	mid = (right-left)/2+left;
+        	hoursTaken = 0;
+        	for(int i = 0; i<piles.length; i++) {
+        		if(piles[i]%mid == 0)
+        			hoursTaken +=piles[i]/mid;
+        		else {
+        			hoursTaken += ( 1  + (piles[i]/mid));
+        		}
+        	}
+        	if(hoursTaken == h) {
+        		result = mid;
+        		right = mid - 1;
+        	}
+        	else if(hoursTaken>h) {
+        		left = mid + 1;
+        	}
+        	else 
+        		right = mid - 1;
+        		
+        }
+    	return result;	
     }
 
 }
