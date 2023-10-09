b1b6bf6a25b2b3b760a3df0bc4192b3f9c985bda
aminef <fouratiamine5@gmail.com>
2023-10-09 16:31:33 +0100

    sync

--- src/main/java/dynamicProgramming/ClimbingStairsN70.java
+++ src/main/java/dynamicProgramming/ClimbingStairsN70.java
@@ -15,4 +15,19 @@ public class ClimbingStairsN70 {
         }
         return stairs[stairs.length - 1];
     }
+    public int climbStairs2(int n) {
+    	if(n == 1)
+    		return 1;
+    	if(n == 2)
+    		return 2;
+        int[] stairs = new int[n];
+        stairs[0] = 1;
+        stairs[1] = 2;
+        for(int i = 2; i < stairs.length-2;i++) {
+        	stairs[i+1]+=stairs[i];
+        	stairs[i+2]+=stairs[i];
+        }
+        stairs[stairs.length - 1] += stairs[stairs.length - 2];
+        return stairs[stairs.length - 1];
+    }
 }
--- src/main/java/dynamicProgramming/MinCostClimbingStairsN746.java
+++ src/main/java/dynamicProgramming/MinCostClimbingStairsN746.java
@@ -1,7 +1,10 @@
 package dynamicProgramming;
 
+import java.util.Arrays;
+
 public class MinCostClimbingStairsN746 {
     
+	//technique#2 (looking backwards)
 	public int minCostClimbingStairs(int[] cost) {
 		int[] minCostTo =  new int[cost.length+1];
 		minCostTo[0] = 0;
@@ -11,5 +14,29 @@ public class MinCostClimbingStairsN746 {
 		}
 		return minCostTo[cost.length];
     }
+	
+	//technique#1 (looking forward)
+	public int minCostClimbingStairs2(int[] cost) {
+		
+		int[] minCostTo = new int[cost.length + 1];
+		Arrays.fill(minCostTo, Integer.MAX_VALUE);
+		minCostTo[0] = 0;
+		minCostTo[1] = 0;
+		
+		for(int i = 0; i < minCostTo.length - 2; i++) {
+			if(minCostTo[i]+cost[i] < minCostTo[i+1])
+				minCostTo[i+1] = minCostTo[i]+cost[i];
+			
+			if(minCostTo[i]+cost[i] < minCostTo[i+2])
+				minCostTo[i+2] = minCostTo[i]+cost[i];
+		}
+		int beforeLastElementIndex = minCostTo.length-2;
+		if(minCostTo[beforeLastElementIndex]+cost[beforeLastElementIndex] < minCostTo[beforeLastElementIndex+1])
+			minCostTo[beforeLastElementIndex+1] = minCostTo[beforeLastElementIndex]+cost[beforeLastElementIndex];
+		
+		return minCostTo[cost.length];
+		
+	}
+	
 
 }
