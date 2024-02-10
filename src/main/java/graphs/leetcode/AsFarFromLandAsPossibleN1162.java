4d9e3e3def4c387374dacaf77a6477dd822adeb2
aminef <fouratiamine5@gmail.com>
2024-02-10 23:04:42 +0100

    leetcode#1162

--- src/main/java/graphs/leetcode/AsFarFromLandAsPossibleN1162.java
+++ src/main/java/graphs/leetcode/AsFarFromLandAsPossibleN1162.java
@@ -11,97 +11,122 @@ import java.util.Set;
 
 public class AsFarFromLandAsPossibleN1162 {
 
-  
-    public int maxDistance(int[][] grid) {
-        boolean foundWater = false;
-        boolean foundLand = false;
-    	for(int i = 0; i < grid.length; i++) {
-    		if(foundWater && foundLand)
-    			break;
-    		for(int j = 0; j < grid.length; j++) {
-    			if(grid[i][j] == 1)
-    				foundLand = true;
-    			if(grid[i][j] == 0)
-    				foundWater = true;
-    		}
-    	}
-    	if(!foundWater || !foundLand)
-    		return -1;
-    	
-    	int currentLargestDistance = -1;
-    	int holder;
-    	Map<String,Integer> waterNodeNearestLandDistanceMap = new HashMap<>();
-    	
-    	for(int i = 0; i < grid.length; i++) {
-    		for(int j = 0; j < grid.length; j++) {
-    			if(grid[i][j] == 0) {
-    				holder = bfsDistance(new int[] {i,j},grid,waterNodeNearestLandDistanceMap);
-    				if(holder > currentLargestDistance)
-    					currentLargestDistance = holder;
-    			}
-    		}
+	public int maxDistanceM1(int[][] grid) {
+		if(invalidGrid(grid))
+			return -1;
+		int longestDistance = 0;
+		int holder;
+		for(int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid.length; j++) {
+				if(grid[i][j] == 1)
+					continue;
+				holder = bfs(grid,i,j);
+				if(longestDistance < holder)
+					longestDistance = holder;
+			}
 		}
-    	return currentLargestDistance;
-    	
-    }
+		return longestDistance;
+
+	}
     
-    public int bfsDistance(int[] originalNode,
-    		int[][] grid,
-    		Map<String,Integer> map ) {
-    	
-    	Set<String> visitedNodes = new HashSet<>();
-    	visitedNodes.add(originalNode[0]+","+originalNode[1]);
-    	Queue<int[]> q = new LinkedList<>();
-    	q.add(originalNode);
-    	List<int[]> currentWave;
-    	int r;
-    	int c;
-    	int currentShortestDistance = Integer.MAX_VALUE;
-    	boolean landFound = false;
-    	while(!q.isEmpty()) {
-    		currentWave = new ArrayList<>();
-    		while(!q.isEmpty()) {
-    			currentWave.add(q.poll());
-    		}
-    		for(int[] node: currentWave) {
-    			r = node[0];
-    			c = node[1];
-    			
-    			if(grid[r][c] == 1 || map.containsKey(r+","+c) ) {
-    				landFound = true;
-    				int distanceHolder = Math.abs(originalNode[0]-r)+Math.abs(originalNode[1]-c);
-    				if(distanceHolder < currentShortestDistance)
-    					currentShortestDistance = distanceHolder;
-    			}
-    				
-    			for(int[] neighbor: new int[][] {{r-1,c+1},{r-1,c},{r-1,c-1},{r,c-1},{r,c+1},{r+1,c-1},{r+1,c},{r+1,c+1}}) {
-    				if(neighbor[0] >= 0 
-    						&& neighbor[0] < grid.length 
-    						&& neighbor[1] >= 0 
-    						&& neighbor[1] < grid.length
-    						&& !visitedNodes.contains(neighbor[0]+","+neighbor[1])) {
-    					visitedNodes.add(neighbor[0]+","+neighbor[1]);
-    					q.add(neighbor);
-    				}
-    			}
-    		}
-    		if(landFound)
-    			return currentShortestDistance;
-    	}
-    	return currentShortestDistance;
-    
-    
-    
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+	public boolean invalidGrid(int[][] grid){
+		boolean noLand = true;
+		boolean noWater = true;
+		for(int i = 0; i < grid.length; i++){
+			if(!noLand && !noWater)
+				return false;
+			for(int j = 0; j < grid.length; j++){
+				if(grid[i][j] == 1)
+					noLand = false;
+				if(grid[i][j] == 0)
+					noWater = false;
+			}
+		}
+		return noLand || noWater;
+	}
+
+
+	public int bfs(int[][] grid, int sourceRow, int sourceColumn){
+		Set<String> visitedCells = new HashSet<>();
+		visitedCells.add(sourceRow+","+sourceColumn);
+		int waveCounter = 0;
+		LinkedList<int[]> waveNodes = new LinkedList<>();
+		waveNodes.addLast(new int[]{sourceRow,sourceColumn});
+		int size,r,c,nbR,nbC;
+		int[] node;
+		while(!waveNodes.isEmpty()){
+			waveCounter++;
+			size = waveNodes.size();
+			for(int i = 0; i < size; i++){
+				node = waveNodes.removeFirst();
+				r = node[0];
+				c = node[1];
+				for(int[] nbNode: new int[][]{{r+1,c},{r-1,c},{r,c+1},{r,c-1}}){
+					nbR = nbNode[0];
+					nbC = nbNode[1];
+					if(nbR < 0 || nbC < 0 || nbR == grid.length || nbC == grid.length)
+						continue;
+					if(visitedCells.contains(nbR+","+nbC))
+						continue;
+					if(grid[nbR][nbC] == 1)
+						return waveCounter;
+					visitedCells.add(nbR+","+nbC);
+					waveNodes.add(new int[]{nbR,nbC});
+
+				}
+			}
+		}
+		return -1;
+	}
+
+
+	public int maxDistance(int[][] grid) {
+
+		LinkedList<int[]> waveNodes = new LinkedList<>();
+		int[] node;
+		int size, waveCounter = 0, r, c, nbR, nbC ;
+		for(int i = 0; i < grid.length; i++){
+			for(int j = 0; j < grid.length; j++){
+				if(grid[i][j] == 1){
+					waveNodes.add(new int[]{i,j});
+				}
+			}
+		}
+
+		if(waveNodes.isEmpty() || waveNodes.size() == grid.length* grid.length)
+			return -1;
+		while(!waveNodes.isEmpty()){
+			size = waveNodes.size();
+			for(int i = 0; i < size; i++){
+				node  = waveNodes.removeFirst();
+				r = node[0];
+				c = node[1];
+				for(int[] nbNode: new int[][]{{r+1,c},{r-1,c},{r,c+1},{r,c-1}}){
+					nbR = nbNode[0];
+					nbC = nbNode[1];
+					if(nbR < 0 || nbC < 0 || nbR == grid.length || nbC == grid.length)
+						continue;
+					if(grid[nbR][nbC] == 1)
+						continue;
+					grid[nbR][nbC] = 1;
+					waveNodes.addLast(new int[]{nbR,nbC});
+
+				}
+			}
+			if(!waveNodes.isEmpty())
+				waveCounter++;
+		}
+		return waveCounter;
+	}
+
+
+
+
+
+
+
+
+
+
     
 }
