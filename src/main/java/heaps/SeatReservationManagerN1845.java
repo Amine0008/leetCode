0f159b7152f946394503ce4de3a1cae49709ac30
aminef <fouratiamine5@gmail.com>
2024-05-18 20:42:52 +0100

    leetcode#1845

---
+++ src/main/java/heaps/SeatReservationManagerN1845.java
@@ -0,0 +1,22 @@
+package heaps;
+
+import java.util.PriorityQueue;
+
+public class SeatReservationManagerN1845 {
+
+    class SeatManager {
+        PriorityQueue<Integer> minHeap;
+        public SeatManager(int n) {
+            minHeap = new PriorityQueue<>();
+            for(int i = 1; i <= n; i++){
+                minHeap.add(i);
+            }
+        }
+        public int reserve() {
+            return minHeap.remove();
+        }
+        public void unreserve(int seatNumber) {
+            minHeap.add(seatNumber);
+        }
+    }
+}
