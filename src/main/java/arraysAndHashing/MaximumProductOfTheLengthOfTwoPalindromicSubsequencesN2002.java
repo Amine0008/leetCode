c6e6d84ec9fda9e6e932ab761b14c877c8f7b6f6
aminef <fouratiamine5@gmail.com>
2024-09-04 00:12:12 +0100

    leetcode#2002

--- src/main/java/arraysAndHashing/MaximumProductOfTheLengthOfTwoPalindromicSubsequencesN2002.java
+++ src/main/java/arraysAndHashing/MaximumProductOfTheLengthOfTwoPalindromicSubsequencesN2002.java
@@ -6,43 +6,48 @@ import java.util.Map;
 public class MaximumProductOfTheLengthOfTwoPalindromicSubsequencesN2002 {
 
     public int maxProduct(String s) {
-        int maxLength = s.length();
-        String binaryRepoHolder;
-        Map<String, Integer> map =  new HashMap<>();
-        for(int i = 1; i < Math.pow(2, s.length()); i++ ){
-            binaryRepoHolder = String.format("%"+maxLength+"s",
-                    Integer.toBinaryString(i)).replace(' ', '0');
-            if(isPalindromic(binaryRepoHolder,s))
-                map.put(binaryRepoHolder, getLength(binaryRepoHolder));
 
+        String subsequenceString;
+        boolean isPalindromic;
+        int inputLength = s.length();
+        Map<Integer, Integer> map = new HashMap<>();
+        int maxProd = 0;
+        for(int mask=0;mask<(1<<inputLength);mask++){// generate bitmask from 1 to 2^n
+
+            subsequenceString = getString(mask, s);
+            isPalindromic = stringIsPalindromic(subsequenceString);
+            if (isPalindromic)
+                map.put(mask, subsequenceString.length());
+
+
+            for (Integer key1 : map.keySet()) {
+                for (Integer key2 : map.keySet()) {
+                    if ((key1 & key2) == 0)
+                        maxProd = Math.max(map.get(key1) * map.get(key2), maxProd);
+                }
+
+            }
         }
+        return maxProd;
     }
 
-    public int getLength(String binaryString) {
-        int counter = 0;
-        for(int i =  0; i < binaryString.length(); i++){
-            if( binaryString.charAt(i) == '1')
-                counter++;
+    String getString(int bitmask, String ogString){
+        String temp="";
+        for(int i=0;i<ogString.length();i++){
+            if((bitmask & (1<<i)) !=0) // generate the string from the mask
+                temp+=ogString.charAt(i);
         }
-        return counter;
+        return temp;
     }
 
-    public boolean isPalindromic(String binaryString, String original){
-        int right = binaryString.length()-1;
-        int left = 0;
-        while(right < left) {
-            while(  right >=0 && binaryString.charAt(right) != '1'){
-                right--;
-            }
-            while( binaryString.charAt(left) != '1'){
-                left++;
-            }
-            if(left >= right)
-                return true;
-            if(original.charAt(left) != original.charAt(right))
+    boolean stringIsPalindromic(String s){
+        int l = 0;
+        int r  = s.length()-1;
+        while(l < r){
+            if(s.charAt(l) != s.charAt(r))
                 return false;
-            left++;
-            right--;
+            l++;
+            r--;
         }
         return true;
     }
